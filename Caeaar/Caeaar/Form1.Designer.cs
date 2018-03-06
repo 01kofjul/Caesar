fc45bd91da48decb6dd32090484a3d5e9a3fcb6a
01kofjul <01kofjul@rgtfo-me.it>
2018-03-06 11:00:08 +0100

    3.Änderung

--- Caeaar/Caeaar/Form1.Designer.cs
+++ Caeaar/Caeaar/Form1.Designer.cs
@@ -29,31 +29,131 @@
         private void InitializeComponent()
         {
             this.btnver = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbin = new System.Windows.Forms.TextBox();
+            this.tbout = new System.Windows.Forms.TextBox();
+            this.tbs = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnentschl = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbent = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnver
             // 
-            this.btnver.Location = new System.Drawing.Point(228, 87);
+            this.btnver.Location = new System.Drawing.Point(262, 197);
             this.btnver.Name = "btnver";
-            this.btnver.Size = new System.Drawing.Size(75, 23);
+            this.btnver.Size = new System.Drawing.Size(86, 23);
             this.btnver.TabIndex = 0;
             this.btnver.Text = "Verschlüsseln";
             this.btnver.UseVisualStyleBackColor = true;
+            this.btnver.Click += new System.EventHandler(this.btnver_Click);
             // 
-            // textBox1
+            // tbin
             // 
-            this.textBox1.Location = new System.Drawing.Point(46, 61);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(448, 20);
-            this.textBox1.TabIndex = 1;
+            this.tbin.Location = new System.Drawing.Point(46, 77);
+            this.tbin.Multiline = true;
+            this.tbin.Name = "tbin";
+            this.tbin.Size = new System.Drawing.Size(302, 98);
+            this.tbin.TabIndex = 1;
+            // 
+            // tbout
+            // 
+            this.tbout.Enabled = false;
+            this.tbout.Location = new System.Drawing.Point(354, 77);
+            this.tbout.Multiline = true;
+            this.tbout.Name = "tbout";
+            this.tbout.Size = new System.Drawing.Size(302, 98);
+            this.tbout.TabIndex = 2;
+            // 
+            // tbs
+            // 
+            this.tbs.Location = new System.Drawing.Point(46, 200);
+            this.tbs.Name = "tbs";
+            this.tbs.Size = new System.Drawing.Size(146, 20);
+            this.tbs.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Schlüssel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Eingabe";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(351, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ausgabe";
+            // 
+            // btnentschl
+            // 
+            this.btnentschl.Location = new System.Drawing.Point(262, 226);
+            this.btnentschl.Name = "btnentschl";
+            this.btnentschl.Size = new System.Drawing.Size(86, 23);
+            this.btnentschl.TabIndex = 7;
+            this.btnentschl.Text = "Entschlüsseln";
+            this.btnentschl.UseVisualStyleBackColor = true;
+            this.btnentschl.Click += new System.EventHandler(this.btnentschl_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(284, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Caesarverschlüsselung";
+            // 
+            // tbent
+            // 
+            this.tbent.Enabled = false;
+            this.tbent.Location = new System.Drawing.Point(46, 291);
+            this.tbent.Multiline = true;
+            this.tbent.Name = "tbent";
+            this.tbent.Size = new System.Drawing.Size(302, 98);
+            this.tbent.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Entschlüsselt";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(710, 479);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbent);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnentschl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbs);
+            this.Controls.Add(this.tbout);
+            this.Controls.Add(this.tbin);
             this.Controls.Add(this.btnver);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -65,7 +165,16 @@
         #endregion
 
         private System.Windows.Forms.Button btnver;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbin;
+        private System.Windows.Forms.TextBox tbout;
+        private System.Windows.Forms.TextBox tbs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnentschl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbent;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Caeaar/Caeaar/Form1.cs
+++ Caeaar/Caeaar/Form1.cs
@@ -15,5 +15,84 @@ namespace Caeaar
         {
             InitializeComponent();
         }
+
+        private void btnver_Click(object sender, EventArgs e)
+        {
+            String klartext = tbin.Text;
+            Console.WriteLine("Geben sie den Schlüssel ein");
+            int schluessel = Convert.ToInt16(tbs.Text);
+
+
+            Char[] chararray = klartext.ToCharArray();
+            Char[] buchstaben = new char[chararray.Length];
+            int[] ascicode = new int[chararray.Length];
+
+            for (int i = 0; i < chararray.Length; i++)
+            {
+                buchstaben[i] = chararray[i];
+                ascicode[i] = Convert.ToInt32(buchstaben[i]);
+                
+            }
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < ascicode.Length; i++)
+            {
+                ascicode[i] = ascicode[i] + schluessel;
+            }
+
+            string ausgabe = "";
+            for (int i = 0; i < chararray.Length; i++)
+            {
+                chararray[i] = Convert.ToChar(ascicode[i]);
+                ausgabe += Convert.ToString(chararray[i]);
+                
+            }
+            tbout.Text = ausgabe;
+        }
+
+        private void btnentschl_Click(object sender, EventArgs e)
+        {
+            String klartext = tbout.Text;
+            Console.WriteLine("Geben sie den Schlüssel ein");
+            int schluessel = Convert.ToInt16(tbs.Text);
+
+            if (schluessel > 0)            
+                schluessel -= (2 * schluessel);
+            
+            else
+            {
+                schluessel -= (2 * schluessel);
+            }
+
+            Char[] chararray = klartext.ToCharArray();
+            Char[] buchstaben = new char[chararray.Length];
+            int[] ascicode = new int[chararray.Length];
+
+            for (int i = 0; i < chararray.Length; i++)
+            {
+                buchstaben[i] = chararray[i];
+                ascicode[i] = Convert.ToInt32(buchstaben[i]);
+
+            }
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < ascicode.Length; i++)
+            {
+                ascicode[i] = ascicode[i] + schluessel;
+            }
+
+            string ausgabe = "";
+            for (int i = 0; i < chararray.Length; i++)
+            {
+                chararray[i] = Convert.ToChar(ascicode[i]);
+                ausgabe += Convert.ToString(chararray[i]);
+
+            }
+            tbent.Text = ausgabe;
+        }
+
+        
     }
 }
